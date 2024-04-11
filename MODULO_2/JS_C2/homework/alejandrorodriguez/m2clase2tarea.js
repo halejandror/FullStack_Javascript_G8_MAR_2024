b2b921b5828338f30c685f3e8d178c1dae6615db
ALEJANDRORODRIGUEZ <usuariolg80@gmail.com>
2024-04-10 23:25:07 -0500

    tareaClase2

---
+++ MODULO_2/JS_C2/homework/alejandrorodriguez/m2clase2tarea.js
@@ -0,0 +1,25 @@
+// var corretPassword = prompt("Ingresa tu contraseña");
+
+// if (corretPassword ==="kambcode"){
+//     alert("Bienvenido a tu Campo de emtrenamiento");
+    
+// }
+//  else {alert("Olvidaste tu contraseña?");
+//  }
+
+
+// var primerNumero= prompt("Dame un numero entero por favor");
+
+// var segundoNumero=  Number(prompt("Dame un segundo numero inferior al anterior"));
+
+// var resultado= primerNumero / segundoNumero;
+
+// if (segundoNumero=== 0) {
+//     alert("error el divisor no pueder ser 0");
+// }
+// if (segundoNumero > primerNumero){
+//     alert("este número debe ser inferior al anterior")
+// }
+// else  {alert("resultado es " + resultado);
+// }
+
