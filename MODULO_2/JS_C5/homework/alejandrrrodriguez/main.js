15ee7ea0e9018e2dc6cf7a34c69fd629bf82d05e
ALEJANDRORODRIGUEZ <usuariolg80@gmail.com>
2024-04-17 21:18:18 -0500

    clase 5

---
+++ MODULO_2/JS_C5/homework/alejandrrrodriguez/main.js
@@ -0,0 +1,19 @@
+// var number= 50;
+
+// while (number <=50 && number >=2){
+//     document.write(number, "<br>");
+//     number = number - 2;
+// }
+// document.write("fin del programa");
+
+// var number= 2;
+
+// while (number <=50 && number >=2){
+//     document.write(number, "<br>");
+//     number = number + 2;
+// }
+// document.write("fin del programa");
+
+for(var number =1; number<=100; number ++){
+    document.write(number, "<br>");
+}
